e9612fcc861e21714994d217455950ab68302471
Ben Vaitkevicius <s1513031@philly.inf.ed.ac.uk>
2019-03-07 19:39:06 +0000

    copy

--- bin/copy-ccKVS-executables.sh
+++ bin/copy-ccKVS-executables.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
-HOSTS=( "austin" "houston" "sanantonio")
+HOSTS=( "philly" "indianapolis" "houston" "sanantonio")
 #HOSTS=( "austin" "houston" "sanantonio")
 #HOSTS=( "austin" "houston" "sanantonio" "indianapolis" "philly" )
 #HOSTS=( "austin" "houston" "sanantonio" "indianapolis" "philly" "baltimore" "chicago" "atlanta" "detroit")
 LOCAL_HOST=`hostname`
-EXECUTABLES=("ccKVS-sc" "ccKVS-lin" "run-ccKVS.sh")
-HOME_FOLDER="/home/user/ccKVS/src/ccKVS"
-DEST_FOLDER="/home/user/ccKVS-exec/src/ccKVS"
-
-cd $HOME_FOLDER
-make
-cd -
+EXECUTABLES=("ccKVS-sc" "run-ccKVS.sh")
+HOME_FOLDER="/home/s1513031/ccKVS/src/ccKVS"
+DEST_FOLDER="/home/s1513031/ccKVS/src/ccKVS"
+if test "$#" -eq 2; then
+	echo "building program"
+	cd $HOME_FOLDER
+	make
+	cd -
+fi
+if test "$#" -eq 0; then
+	echo "usage ./copy-executables password (make)"
+	exit
+fi
 
 for EXEC in "${EXECUTABLES[@]}"
 do
-	#echo "${EXEC} copied to {${HOSTS[@]/$LOCAL_HOST}}"
-	parallel scp ${HOME_FOLDER}/${EXEC} {}:${DEST_FOLDER}/${EXEC} ::: $(echo ${HOSTS[@]/$LOCAL_HOST})
-	echo "${EXEC} copied to {${HOSTS[@]/$LOCAL_HOST}}"
+	for HOST in "${HOSTS[@]}"
+	do
+		#echo "making directories"
+		#sshpass -p "$1" ssh ${HOST}
+		#mkdir ${DEST_FOLDER}
+		#exit
+		echo "${EXEC} copying to ${HOST}:/${DEST_FOLDER} "
+		sshpass -p "$1" scp ${HOME_FOLDER}/${EXEC} ${HOST}:${DEST_FOLDER}/${EXEC}
+		#echo "${EXEC} copied to {${HOSTS[@]/$LOCAL_HOST}}"
+
+	done
 done
